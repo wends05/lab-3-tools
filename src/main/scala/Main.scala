764f5d11a024a6b050bd7784487c2518d9c07080
wends05 <cxnner05@gmail.com>
2024-09-18 15:45:56 +0800

    adding fixes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,23 @@
+
 @main def hello(): Unit =
-  println("Hello world!")
   printLyrics(close_to_you_gracie)
 
-type Lyrics = Map[Double, String]
+type Lyrics = List[(Double, String)]
 
 def printLyrics(lyrics: Lyrics) =
-  lyrics.foreach((delay_per_word, line) =>
-    println(delay_per_word.toFloat * 1000)
-    Thread.sleep((delay_per_word.toLong * 1000))
-    println(line)
-  )
 
-val close_to_you_gracie : Lyrics = Map(
-  0.5 -> "Pull the trigger on the gun",
-  0.5 -> "I gave you when we met",
-  0.5 -> "I want to be",
-  0.5 -> "Close to you",
-  1.0 -> "Break my heart and start a fire",
-  0.6 -> "You've got me overnight",
-  0.5 -> "Just let me be",
-  1.0 -> "Close to you",
+  for ((time, line) <- lyrics) do
+    println(line)
+    Thread.sleep((time * 1000).toLong)
+  
+
+val close_to_you_gracie = List(
+  1.8 -> "Pull the trigger on the gun",
+  1.8 -> "I gave you when we met",
+  1.7 -> "I want to be",
+  2.3 -> "Close to you",
+  1.8 -> "Break my heart and start a fire",
+  1.7 -> "You've got me overnight",
+  1.8 -> "Just let me be",
+  2.0 -> "Close to you",
 )
