d8257fdf96344d3940dd67cf648a90d92b2d4159
wends05 <cxnner05@gmail.com>
2024-09-18 14:37:56 +0800

    added a new function for displaying lyrics

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,3 +3,16 @@
   println(msg)
 
 def msg = "I was compiled by Scala 3. :)"
+
+
+type Lyrics = Map[List[Float], String]
+
+
+def lyric_displayer(lyrics: Lyrics) = 
+  for ((word_delay, line_delay), line) <- lyrics do
+    for character <- line do
+      print(character)
+      Thread.sleep((word_delay * 1000).toLong)
+
+
+
