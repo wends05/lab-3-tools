af6da9050641913b9fa374c82bf2bef35225e1c7
wends05 <cxnner05@gmail.com>
2024-09-18 16:00:52 +0800

    Added delay per line

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,22 +2,25 @@
 @main def hello(): Unit =
   printLyrics(close_to_you_gracie)
 
-type Lyrics = List[(Double, String)]
+type Lyrics = List[((Double, Double), String)]
 
 def printLyrics(lyrics: Lyrics) =
 
-  for ((time, line) <- lyrics) do
-    println(line)
-    Thread.sleep((time * 1000).toLong)
+  for (((delay_per_character, delay_after_line), line) <- lyrics) do
+    for character <- line do
+      print(character)
+      Thread.sleep((delay_per_character * 1000).toLong)
+    println("")
+    Thread.sleep((delay_after_line * 1000).toLong)
   
 
 val close_to_you_gracie = List(
-  1.8 -> "Pull the trigger on the gun",
-  1.8 -> "I gave you when we met",
-  1.7 -> "I want to be",
-  2.3 -> "Close to you",
-  1.8 -> "Break my heart and start a fire",
-  1.7 -> "You've got me overnight",
-  1.8 -> "Just let me be",
-  2.0 -> "Close to you",
+  (0.05, 0.5) -> "Pull the trigger on the gun",
+  (0.1, 0.5) -> "I gave you when we met",
+  (0.1, 0.5) -> "I want to be",
+  (0.2, 0.5) -> "Close to you",
+  (0.1, 0.5) -> "Break my heart and start a fire",
+  (0.1, 0.5) -> "You've got me overnight",
+  (0.1, 0.5)-> "Just let me be",
+  (0.1, 0.5) -> "Close to you",
 )
