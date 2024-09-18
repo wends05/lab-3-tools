dd6e8cadfb2cf22b88a9b76212d968a9d3b66d0b
wends05 <cxnner05@gmail.com>
2024-09-18 16:54:45 +0800

    remade 2nd song lyrics

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -15,23 +15,25 @@ def printLyrics(lyrics: Lyrics) =
   
 
 val close_to_you_gracie = List(
-  (0.05, 0.5) -> "Pull the trigger on the gun",
-  (0.1, 0.5) -> "I gave you when we met",
-  (0.1, 0.5) -> "I want to be",
-  (0.2, 0.5) -> "Close to you",
-  (0.1, 0.5) -> "Break my heart and start a fire",
-  (0.1, 0.5) -> "You've got me overnight",
-  (0.1, 0.5)-> "Just let me be",
-  (0.1, 0.5) -> "Close to you",
+  (0.05, 0.4) -> "Pull the trigger on the gun",
+  (0.05, 0.5) -> "I gave you when we met",
+  (0.05, 0.7) -> "I want to be",
+  (0.07, 1.5) -> "Close to you",
+  (0.04, 0.4) -> "Break my heart and start a fire",
+  (0.05, 0.5) -> "You've got me overnight",
+  (0.05, 0.5) -> "Just let me be",
+  (0.07, 0.5) -> "Close to you",
 )
 
-val so_long_london_taylor : Lyrics = List(
-  0.5 -> "And you say I abandoned the ship",
-  0.5 -> "But I was going down with it",
-  0.5 -> "My white-knuckle dying grip",
-  0.5 -> "Holding tight to your quiet resentment",
-  0.5 -> "And my friends said it isn't right to be scared",
-  0.5 -> "Every day of a love affair",
-  0.5 -> "Every breath feels like rarest air",
-  0.5 -> "When you're not sure if he wants to be there"
+val so_long_london_taylor = List(
+  (0.05, 0.1) -> "And you say I abandoned the ship",
+  (0.05, 0.4) -> "But I",
+  (0.05, 1.0) -> "was going down with it",
+  (0.05, 0.5) -> "My white-",
+  (0.05, 1.2) -> "knuckle dying grip",
+  (0.05, 0.3) -> "Holding tight to your quiet resentment",
+  (0.05, 0.2) -> "And my friends said it isn't right to be scared",
+  (0.05, 1.2) -> "Every day of a love affair",
+  (0.05, 1.2) -> "Every breath feels like rarest air",
+  (0.05, 0.5) -> "When you're not sure if he wants to be there"
 )
