d6d1ddac0a7812b3ac2e7e24bd6b08d55f4c18df
wends05 <cxnner05@gmail.com>
2024-09-18 15:06:07 +0800

    initial project progress

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,23 @@
 @main def hello(): Unit =
   println("Hello world!")
-  println(msg)
+  printLyrics(close_to_you_gracie)
 
-def msg = "I was compiled by Scala 3. :)"
+type Lyrics = Map[Double, String]
+
+def printLyrics(lyrics: Lyrics) =
+  lyrics.foreach((delay_per_word, line) =>
+    println(delay_per_word.toFloat * 1000)
+    Thread.sleep((delay_per_word.toLong * 1000))
+    println(line)
+  )
+
+val close_to_you_gracie : Lyrics = Map(
+  0.5 -> "Pull the trigger on the gun",
+  0.5 -> "I gave you when we met",
+  0.5 -> "I want to be",
+  0.5 -> "Close to you",
+  1.0 -> "Break my heart and start a fire",
+  0.6 -> "You've got me overnight",
+  0.5 -> "Just let me be",
+  1.0 -> "Close to you",
+)
